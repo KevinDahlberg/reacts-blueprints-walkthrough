3bfec17194cfc2e6fad29eea5b6cf03adb6f0061
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-09-20 00:02:38 -0500

    built footer

---
+++ web-shop/src/components/footer.jsx
@@ -0,0 +1,15 @@
+'use strict';
+import React from 'react';
+
+const Footer = React createClass({
+  render() {
+    return (
+      <footer className="footer text-center">
+        <div className="container">
+          <p className="text-muted">Copyright 2017 Awesome Webshop</p>
+        </div>
+      </footer>
+    );
+  }
+});
+module.exports = Footer;
