e8c0d40a2eb2f0b9d2bdf3302d9ef0095be505de
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-09-23 21:18:11 -0500

    finished item.jsx

--- web-shop/src/pages/item.jsx
+++ web-shop/src/pages/item.jsx
@@ -4,6 +4,7 @@ import Reflux from 'reflux';
 import { Router, State } from 'react-router-dom';
 import { Grid, Row, Col, Button } from 'react-bootstrap';
 import CartActions from "../actions/cart";
+import PropTypes from 'prop-types';
 
 
 export default class Item extends Reflux.Component {
@@ -44,3 +45,46 @@ export default class Item extends Reflux.Component {
       };
     }
   }
+
+class ProductInfo extends Component {
+  static PropTypes = {
+    productData: React.PropTypes.object
+  }
+
+  render() {
+    const title = Object.keys(this.props.productData);
+    if(this.props.productData[title]){
+      (
+        <Col xs={12}>
+          <Col md={3} sm={4} xs={12}>
+            <p>
+              <img src={this.props.productData[title].image.replace("{size}","200x150")}/>
+            </p>
+          </Col>
+          <Col md={9} sm={8} xs={12}>
+            <h4>{title}</h4>
+            <p>
+              {this.props.productData[title].description}
+            </p>
+
+            <p>
+              {this.props.productData[title].price}
+              {" "}
+              ({this.props.productData[title].savings})
+            </p>
+
+            <p>
+              <Button bsSize="large"
+                onClick={CartActions.AddToCart.
+                bind(null, this.props.productData)}>
+                Add to cart
+              </Button>
+            </p>
+          </Col>
+        </Col>
+      )
+    } else {
+      return null;
+    }
+  }
+}
