61298231aea58de0125c2a997018aeac060f958d
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-09-23 20:54:14 -0500

    finished products.jsx

--- web-shop/src/pages/item.jsx
+++ web-shop/src/pages/item.jsx
@@ -1,10 +1,46 @@
 'use strict';
 import React, { Component } from 'react';
+import Reflux from 'reflux';
+import { Router, State } from 'react-router-dom';
+import { Grid, Row, Col, Button } from 'react-bootstrap';
+import CartActions from "../actions/cart";
 
-export default class Item extends Component {
-  render() {
-    return (
-      <div />
-    );
+
+export default class Item extends Reflux.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
-};
+  render() {
+    if (!this.props.products) return null;
+
+    let products = this.props.products.main_offering.concat(this.props.products.sale_offerings);
+    let data = products.filter((item) => {
+      return item[Object.keys(item)].SKU === this.props.routeParams.id;
+    });
+
+    if(!data.length) {
+      return (
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <h1>Product missing</h1>
+              <p>
+                I'm sorry, but the product could not be found.
+              </p>
+            </Col>
+          </Row>
+        </Grid>
+      )} else {
+        return (
+          <Grid>
+            <Row>
+              <Col xs={12}>
+                <ProductInfo productData={data[0]}/>
+              </Col>
+            </Row>
+          </Grid>
+        )
+      };
+    }
+  }
